333345bbc23557155953bf4039367f40e052048c
Alexuxu <1214852168@qq.com>
2018-11-30 11:30:03 +0800

    bayesian half done

--- bayesian.py
+++ bayesian.py
@@ -13,6 +13,7 @@ def calcu_possi(data):
 
 
 def bayes(data):
+    # 将数据分类
     classified_data = dict()
     for i in data:
         if i[-1] not in classified_data:
@@ -22,15 +23,42 @@ def bayes(data):
         else:
             classified_data[i[-1]].append(i)
 
+    # 将数据依次求先验概率
     classified_possi = dict()
     for i in classified_data:
         t = np.array(classified_data[i]).T.tolist()
-        possi_list = list()
+        possi_dict = dict()
         for j in t[:-1]:
-            possi_list.append(calcu_possi(j))
+            possi_dict = dict(possi_dict, **calcu_possi(j))
 
-        classified_possi[i] = possi_list
-    print(classified_possi)
+        classified_possi[i] = possi_dict
+
+    # 求类别的先验概率
+    clas_possi = calcu_possi(np.array(data).T.tolist()[-1])
+
+    # 储存所有属性可能取值
+    values = set()
+    for i in data:
+        for j in i[:-1]:
+            if j not in values:
+                values.add(j)
+
+    # 最终结果
+    final_dict = dict()
+    for value in values:
+        t_dict = dict()
+        for clas, dic in zip(classified_possi.keys(), classified_possi.values()):
+            if clas not in t_dict:
+                if value in dic:
+                    t_dict[clas] = dic[value] * clas_possi[clas]
+                else:
+                    t_dict[clas] = 0
+
+        possi_sum = sum(t_dict.values())
+        for i in t_dict:
+            t_dict[i] = t_dict[i]/possi_sum
+        final_dict[value] = t_dict
+    print(final_dict)
 
 
 def import_data(filename, reverse=False):
